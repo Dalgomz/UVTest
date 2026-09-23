0ffb88a689ce4a67069aea4682eefba63e0621fd
leo_kf <leonardo.gomez@klearfinance.com>
2026-09-23 06:38:57 +0200

    Added Django app and endpoint definition

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path
+from kpis import views as kpi_views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("health/", kpi_views.health, name="health"),
+    path("kpi/", kpi_views.kpi_from_polygon, name="kpi_from_polygon"),
 ]
