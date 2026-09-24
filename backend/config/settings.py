421cead253be90959181b4c89213cb55d6262d2f
Leonardo Gomez <33590258+Dalgomz@users.noreply.github.com>
2026-09-24 23:45:56 +0000

    Added docker file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wvh!k5u8&f5uh%r@=mg11kr80y^ewu*!m==ibob3)$gamcqka4"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "1") == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
