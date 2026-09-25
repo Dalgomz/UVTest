c5d30f537edbb72871608709b8099bf3df698454
leo_kf <leonardo.gomez@klearfinance.com>
2026-09-25 06:38:03 +0200

    Removed unused files

--- scripts/overture_extraction.sh
+++
@@ -1,9 +0,0 @@
-
-echo "Checking / Installing overture CLI"
-pip show overturemaps > /dev/null 2>&1 || pip install overturemaps
-
-overturemaps download --bbox=9.194334,45.471917,9.218495,45.487082 -f geojson --type=transportation -o milano_transportation.geojson
-overturemaps download --bbox=9.194334,45.471917,9.218495,45.487082 -f geojson --type=place -o milano_place.geojson
-overturemaps download --bbox=9.194334,45.471917,9.218495,45.487082 -f geojson --type=building -o milano_building.geojson
-overturemaps download --bbox=9.194334,45.471917,9.218495,45.487082 -f geojson --type=land,land_use -o milano_infrastructure.geojson
-overturemaps download --bbox=9.194334,45.471917,9.218495,45.487082 -f geojson --type=infrastructure -o milano_infrastructure.geojson
